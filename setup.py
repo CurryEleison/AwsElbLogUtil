d1af135eb95ae64c6a1c6b324255ae060a3b75b5
Thomas Petersen <thomas@simplesite.com>
2016-12-23 09:49:31 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='AwsLogUtils',
+        version='0.01',
+        description='Library to help downloading and parsing logs from AWS ELB',
+        url='https://github.com/CurryEleison/AwsElbLogUtil',
+        author='Thomas Petersen',
+        author_email='petersen@temp.dk',
+        license='MIT',
+        packages=['AwsLogUtils'],
+        zip_safe=False)
+
