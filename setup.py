32e78ba65e3d58fe5f0075d75afd1b2601a88647
Thomas Petersen <thomas@simplesite.com>
2016-12-27 15:55:30 +0000

    Put requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+        required = f.read().splitlines()
+
 setup(name='AwsElbLogUtil',
         version='0.1',
         description='Library to help downloading and parsing logs from AWS ELB',
@@ -8,5 +11,6 @@ setup(name='AwsElbLogUtil',
         author_email='petersen@temp.dk',
         license='MIT',
         packages=['AwsElbLogUtil'],
+        install_requires=required,
         zip_safe=False)
 
